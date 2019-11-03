6ec06718c95be9ae53dccef8d2ede9daa4afa162
Nerius Perez <nerius.perez@globant.com>
2019-11-03 12:36:43 -0300

    primer proyecto variables

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project-c
 Estudio de C
+https://inducesmile.com/c-programming/hello-word-program-in-c-with-codeblocks/
---
+++ projecto/main.c
@@ -0,0 +1,17 @@
+
+/* Prints a message on the screen */
+#include <stdio.h>
+main()
+{
+printf("Just one small step for coders. One giant leap for\n");
+printf(" programmers!\n");
+
+
+printf("I am learning the %c programming language\n", 'C');
+printf("I have just completed Chapter %d\n", 2);
+printf("I am %.1f percent ready to move on ", 99.9);
+printf("to the next chapter!\n");
+
+
+return 0;
+}
